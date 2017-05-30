394dca248edc95a13589ad068cbd25dd1c7a1d2d
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-05-30 16:08:30 -0700

    wave 1 and part of wave 3

--- src/app.js
+++ src/app.js
@@ -2,8 +2,12 @@ import _ from 'underscore';
 import $ from 'jquery';
 import 'jquery-colpick';
 
+import Postit from './models/postit';
 
-var postitData = [{
+var postitTemplate;
+
+var postitData = [
+  {
     text: "Backbone is a library not a Framework.",
     color: "#AC1200"
 },
@@ -12,8 +16,47 @@ var postitData = [{
     color: "#752310"
 }];
 
+// var myPostit = new Postit(postitData[0]);
+
+var readPostitForm = function() {
+  var textData = $("#text").val();
+  $("#text").val("");
+
+  var colorData = $("#color").val();
+  $("#color").val("#FFFF88");
+
+  var formData = {};
+  if (textData && textData != "") {
+    formData["text"] = textData
+  }
+  if (colorData && colorData != "") {
+    formData["color"] = colorData
+  }
+  return formData;
+};
+
+var render = function(postit) {
+  var jsonPostit = postit.toJSON();
+  var generatedHTML = postitTemplate(jsonPostit);
+  console.log(generatedHTML);
+
+  $("#postits").append(generatedHTML);
+};
+
 $(document).ready(function() {
   console.log("Lets go!");
+
+  postitTemplate  = _.template($("#postit-template").html());
+
+  $(".success").click(function(event) {
+    var formData = readPostitForm();
+    console.log(formData);
+
+    var postit = new Postit(formData);
+    render(postit);
+  });
+
+  // render(myPostit);
 });
 
 // end
--- src/models/postit.js
+++ src/models/postit.js
@@ -1,2 +1,12 @@
 // postit.js
 import Backbone from 'backbone';
+
+var Postit = Backbone.Model.extend({
+  defaults: {
+    "text": "blank note",
+    // "color": "yellow"
+    // "size": 8
+  }
+});
+
+export default Postit;
